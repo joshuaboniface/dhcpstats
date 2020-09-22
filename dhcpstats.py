7961b94abd47b94d790a2ae8519bc4c815e1fcf2
Joshua M. Boniface <joshua@boniface.me>
2020-09-21 20:10:30 -0400

    Add debug logging and fix semicolon issue

--- dhcpstats.py
+++ dhcpstats.py
@@ -71,9 +71,13 @@ def get_subnets(subnet_file, static_file, leases_file):
     used and total number of IPs in the range.
     """
     # Read in the subnet_file
+    if debug:
+        print('Opening subnet file...')
     with open(subnet_file, 'r') as subnets_fh:
         subnets_raw = subnets_fh.read().split('\n')
 
+    if debug:
+        print('Parsing subnets...')
     subnets = dict()
     in_subnet_block = False
     in_pool_block = False
@@ -100,8 +104,8 @@ def get_subnets(subnet_file, static_file, leases_file):
             if in_pool_block:
                 # A range line
                 if re.match('^\s*range', line):
-                    range_start = ipaddress.ip_address(line_split[1])
-                    range_end = ipaddress.ip_address(line_split[2])
+                    range_start = ipaddress.ip_address(re.sub(';', '', line_split[1]))
+                    range_end = ipaddress.ip_address(re.sub(';', '', line_split[2]))
                     range_length = int(range_end) - int(range_start) + 1
 
                     subnets[current_subnet.with_prefixlen]['ranges'].append([str(range_start), str(range_end)])
@@ -148,9 +152,13 @@ def get_subnets(subnet_file, static_file, leases_file):
             subnets[subnet.with_prefixlen]['ips']['static'] = 0
 
     # Read in the leases_file
+    if debug:
+        print('Opening leases file...')
     with open(leases_file, 'r') as leases_fh:
         leases_raw = leases_fh.read().split('\n')
 
+    if debug:
+        print('Parsing leases...')
     leases = dict()
     in_lease_block = False
     current_lease = ''
@@ -195,9 +203,13 @@ def get_subnets(subnet_file, static_file, leases_file):
             leases[str(lease)] = dict()
 
     # Read in the static entries
+    if debug:
+        print('Opening static file...')
     with open(static_file, 'r') as static_fh:
         statics_raw = static_fh.read().split('\n')
 
+    if debug:
+        print('Parsing statics...')
     statics = dict()
     in_host_block = False
     current_static = ''
@@ -253,6 +265,8 @@ def get_subnets(subnet_file, static_file, leases_file):
                 statics[name] = {}
 
     # We now have a full dictionary of subnets and of leases; combine them into a final data structure
+    if debug:
+        print('Combining and counting leases...')
     for lease in leases:
         lease_ipobj = ipaddress.ip_address(lease)
         for subnet in subnets:
@@ -269,6 +283,8 @@ def get_subnets(subnet_file, static_file, leases_file):
                 subnets[lease_subnet]['ips']['backup'] += 1
             subnets[lease_subnet]['leases'][lease] = leases[lease]
 
+    if debug:
+        print('Combining and counting statics...')
     for static in statics:
         static_ipobj = ipaddress.ip_address(statics[static]['ip_address'])
         for subnet in subnets:
@@ -279,11 +295,15 @@ def get_subnets(subnet_file, static_file, leases_file):
             subnets[static_subnet]['statics'][static] = statics[static]
             subnets[static_subnet]['ips']['static'] += 1
 
+    if debug:
+        print('Combining and counting subnets...')
     for subnet in subnets:
         subnet_used = subnets[subnet]['ips']['active'] + subnets[subnet]['ips']['free'] + subnets[subnet]['ips']['backup']
         subnet_unused = subnets[subnet]['ips']['total'] - subnet_used
         subnets[subnet]['ips']['unused'] = subnet_unused
 
+    if debug:
+        print('Returning...')
     retdata = subnets
     retcode = 200
     return retdata, retcode
